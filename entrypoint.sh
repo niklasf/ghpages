6133d697bcc421b4bf009c5bb5e96b1a70eaf18d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2022-04-21 12:06:59 +0200

    explicitly use initial branch main

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,11 @@ REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)" && \
 OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)" && \
 GHIO="${OWNER}.github.io" && \
 if [[ "$REPONAME" == "$GHIO" ]]; then
-  REMOTE_BRANCH="master"
+  REMOTE_BRANCH="main"
 else
   REMOTE_BRANCH="gh-pages"
 fi && \
-git init && \
+git init --initial-branch main && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 if [ -z "$(git status --porcelain)" ]; then
@@ -26,7 +26,7 @@ if [ -z "$(git status --porcelain)" ]; then
 fi && \
 git add . && \
 git commit -m 'Deploy to GitHub Pages' && \
-git push --force $REMOTE_REPO master:$REMOTE_BRANCH && \
+git push --force $REMOTE_REPO main:$REMOTE_BRANCH && \
 rm -fr .git && \
 cd $GITHUB_WORKSPACE && \
 echo "Content of $BUILD_DIR has been deployed to GitHub Pages."
